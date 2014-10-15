f44f4699fa01f5f3631a7596e2a20076d0875343
GariceZa <garethpriceza@gmail.com>
2014-10-15 15:14:14 +0200

    added functionality to pull images from the db to the image placeholders removed unused nav-tabs

--- home.php
+++ home.php
@@ -1,63 +1,98 @@
+include_once 'include/db.php';
+//creating new db object
+$db = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_DATABASE);
+ 
+//checking the connection to the db before user tries to add a new user
+if($db -> connect_errno > 0){
+                //setting the global variable to the error message
+                $GLOBALS['Error'] = 'Unable to connect to database ['.$db->connect_error.']';   
+}
+                
+function displayImages($db){
+ 
+                $sql = "SELECT animal_ID FROM tbl_Animals ORDER BY animal_ID DESC LIMIT 1";
+                $startAnimalID = $db->query($sql)->fetch_row()[0];
+                
+                for($count = 0;$count<=11;$count++){
+                                $sql = "SELECT animal_Image FROM tbl_Animals WHERE animal_ID = $startAnimalID";
+                                $imgArr[$count] = $db->query($sql)->fetch_row()[0];
+                                $startAnimalID--;
+                }
+                
+                echo"<div class=\"container\" id=\"pics\">";                                                      
+                echo"<div class=\"post-box col-lg-3 col-md-3 col-xs-4\"> <img class=\"img-responsive img-thumbnail\"src='data:image/jpg;base64," . base64_encode($imgArr[0]) . "' alt = 'image'/></div>";
+                echo"<div class=\"post-box col-lg-3 col-md-3 col-xs-4\"> <img class=\"img-responsive img-thumbnail\"src='data:image/jpg;base64," . base64_encode($imgArr[1]) . "' alt = 'image'/></div>";
+                echo"<div class=\"post-box col-lg-3 col-md-3 col-xs-4\"> <img class=\"img-responsive img-thumbnail\"src='data:image/jpg;base64," . base64_encode($imgArr[2]) . "' alt = 'image'/></div>";
+                
+                echo"<div class=\"post-box col-lg-3 col-md-3 col-xs-4\"> <img class=\"img-responsive img-thumbnail\"src='data:image/jpg;base64," . base64_encode($imgArr[3]) . "' alt = 'image'/></div>";
+                echo"<div class=\"post-box col-lg-3 col-md-3 col-xs-4\"> <img class=\"img-responsive img-thumbnail\"src='data:image/jpg;base64," . base64_encode($imgArr[4]) . "' alt = 'image'/></div>";
+                echo"<div class=\"post-box col-lg-3 col-md-3 col-xs-4\"> <img class=\"img-responsive img-thumbnail\"src='data:image/jpg;base64," . base64_encode($imgArr[5]) . "' alt = 'image'/></div>";
+                
+                echo"<div class=\"post-box col-lg-3 col-md-3 col-xs-4\"> <img class=\"img-responsive img-thumbnail\"src='data:image/jpg;base64," . base64_encode($imgArr[6]) . "' alt = 'image'/></div>";
+                echo"<div class=\"post-box col-lg-3 col-md-3 col-xs-4\"> <img class=\"img-responsive img-thumbnail\"src='data:image/jpg;base64," . base64_encode($imgArr[7]) . "' alt = 'image'/></div>";
+                echo"<div class=\"post-box col-lg-3 col-md-3 col-xs-4\"> <img class=\"img-responsive img-thumbnail\"src='data:image/jpg;base64," . base64_encode($imgArr[8]) . "' alt = 'image'/></div>";
+                
+                echo"<div class=\"post-box col-lg-3 col-md-3 col-xs-4\"> <img class=\"img-responsive img-thumbnail\"src='data:image/jpg;base64," . base64_encode($imgArr[9]) . "' alt = 'image'/></div>";
+                echo"<div class=\"post-box col-lg-3 col-md-3 col-xs-4\"> <img class=\"img-responsive img-thumbnail\"src='data:image/jpg;base64," . base64_encode($imgArr[10]) . "' alt = 'image'/></div>";
+                echo"<div class=\"post-box col-lg-3 col-md-3 col-xs-4\"> <img class=\"img-responsive img-thumbnail\"src='data:image/jpg;base64," . base64_encode($imgArr[11]) . "' alt = 'image'/></div>";
+                echo"</div>";
+}
+                                                
+ 
+?>
 <!DOCTYPE PUBLIC>
 <html>
-	<head>
-		<title>Animal Management</title>
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/mycss.css">
-	<head/>
-	<body>
-		<div class="container">
-			<div class="jumbotron">
-				<img src="images/aacl.png" alt="" class="pic image-responsive" align="left">
-				<h1>Animal Anti-Cruelty League</h1>
-				<h5>Est. 1956</h5>				
-			</div>
+                <head>
+                                <title>Animal Management</title>
+                                <link rel="stylesheet" href="css/bootstrap.min.css">
+                                <link rel="stylesheet" href="css/mycss.css">
+                                <link rel="stylesheet" href="css/mycss.css">
+                                                <link rel="icon" 
+                  type="image/ico" 
+                  href="images/favicon.ico">
+                <head/>
+                <body>
+                                <div class="container">
+                                                <div class="jumbotron">
+                                                                <img src="images/aacl.png" alt="" class="pic image-responsive" align="left">
+                                                                <h1>Animal Anti-Cruelty League</h1>
+                                                                <h5>Est. 1956</h5>                                                        
+                                                </div>
 <!----------------------------------------------------------//ROW//---------------------------------------------------------------------------------->
-
-			<div class="row">
-				<div class="col-xs-12 col-md-12"> 
-					<ul class="nav nav-tabs" data-tabs="tabs" >
-			  			<li class="active" data-toggle="tab"><a href="home.html" >Home</a></li>		  			
-			  			<li><a href="animals.php">Animals</a></li>
-			  			<li><a href="qrcode.php">QR Codes</a></li>
-			  			<li><a href="users.php">Users</a></li>
-			  			<li><a href="inspectors.php">Inspectors</a></li>
-			  			<li><a href="reportsd.php">Reports</a></li>
-			  			<!--<li><a href="gallery.html">Gallery</a></li>
-			  			<li ><a href="help.html">Help</a></li>-->
-					</ul> 
-				</div> 					
-			</div>	
+                                
+                                                <div class="row">
+                                                                <div class="col-xs-12 col-md-12"> 
+                                                                                <ul class="nav nav-tabs" data-tabs="tabs" >
+                                                                                                <li class="active" data-toggle="tab"><a href="home.php" >Home</a></li>                                                                                
+                                                                                                <li><a href="animals.php">Animals</a></li>
+                                                                                                <li><a href="qrcode.php">QR Codes</a></li>
+                                                                                                <li><a href="users.php">Users</a></li>
+                                                                                                <li><a href="inspectors.php">Inspectors</a></li>
+                                                                                                <li><a href="reportsd.php">Reports</a></li>
+                                                                                </ul> 
+                                                                </div>                                                                   
+                                                </div>   
 <!----------------------------------------------------------//ROW//---------------------------------------------------------------------------------->
-			<div class="row">				
-				<div class="col-xs-3 col-md-4">
-				</div>
-				<div class="col-xs-6 col-md-3">
-					<h2>Welcome</h2>					
-				</div>				
-			</div>
-			<div class="row">	
-			<div class="col-xs-2 col-md-2">
-				</div>			
-				<div class="col-xs-12 col-md-7">
-					<p class="colour" align="center"> These were the last 12 animals entered into or updated on the Animal Management System Database.</p>
-				</div>
-			</div>
-<!----------------------------------------------------------//ROW//---------------------------------------------------------------------------------->			
-			<div class="container" id="pics">				
-				<div class="post-box col-lg-3 col-md-3 col-xs-4" ><img class="img-responsive img-thumbnail" src="images/Harry.jpg"/></div> 
-				<div class="post-box col-lg-3 col-md-3 col-xs-4"> <img class="img-responsive img-thumbnail" src="images/doggie.jpg"/></div>
-				<div class="post-box col-lg-3 col-md-3 col-xs-4"> <img class="img-responsive img-thumbnail" src="images/images.jpg"/></div>
-				<div class="post-box col-lg-3 col-md-3 col-xs-4"> <img class="img-responsive img-thumbnail" src="images/blackdog.jpg"/></div>
-				<div class="post-box col-lg-3 col-md-3 col-xs-4"> <img class="img-responsive img-thumbnail" src="images/kitteh.jpeg"/></div>
-				<div class="post-box col-lg-3 col-md-3 col-xs-4"> <img class="img-responsive img-thumbnail" src="images/ginger.jpg"/></div>		
-				<div class="post-box col-lg-3 col-md-3 col-xs-4"> <img class="img-responsive img-thumbnail" src="images/sweet.jpg"/></div>		
-				<div class="post-box col-lg-3 col-md-3 col-xs-4"> <img class="img-responsive img-thumbnail" src="images/cutie.jpg"/></div>		
-				<div class="post-box col-lg-3 col-md-3 col-xs-4"> <img class="img-responsive img-thumbnail" src="images/cute.jpg"/></div>		
-				<div class="post-box col-lg-3 col-md-3 col-xs-4"> <img class="img-responsive img-thumbnail" src="images/multeys.jpg"/></div>		
-				<div class="post-box col-lg-3 col-md-3 col-xs-4"> <img class="img-responsive img-thumbnail" src="images/pugtatoe.jpg"/></div>		
-				<div class="post-box col-lg-3 col-md-3 col-xs-4"> <img class="img-responsive img-thumbnail" src="images/precious.jpg"/></div>						
-			</div>		
-		</div>
-	</body>
+                                                <div class="row">                                                            
+                                                                <div class="col-xs-3 col-md-4">
+                                                                </div>
+                                                                <div class="col-xs-6 col-md-3">
+                                                                                <h2>Welcome</h2>                                                                      
+                                                                </div>                                                   
+                                                </div>
+                                                <div class="row">            
+                                                <div class="col-xs-2 col-md-2">
+                                                                </div>                                   
+                                                                <div class="col-xs-12 col-md-7">
+                                                                                <p class="colour" align="center"> These were the last 12 animals entered into or updated on the Animal Management System Database.</p>
+                                                                </div>
+                                                </div>
+<!----------------------------------------------------------//ROW//---------------------------------------------------------------------------------->       
+<?php
+displayImages($db);
+?>           
+                
+                                </div>
+                </body>
 </html>
+ 
